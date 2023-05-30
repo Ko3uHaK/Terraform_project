e2e6721df22d6e19e2e5073dd09d7fbc015d058d
daniil.kachura <daniil.kachura@innowise-group.com>
2023-05-30 15:41:37 +0300

    stage(1.1)

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ module "resource-group" {
   location            = var.location
   resource_group_name = var.resource_group_name
 }
+module "virtual-network" {
+  source                = "./modules/virtual-network/"
+  virtual_network_name  = var.virtual_network_name
+  address_space         = var.address_space
+  location              = module.resource-group.location
+  resource_group_name   = module.resource-group.name
+}
--- variables.tf
+++ variables.tf
@@ -7,6 +7,17 @@ variable "resource_group_name" {
   description = "The name of the resource group."
   default     = "rg-eastus-ci"
 }
+variable "virtual_network_name" {
+  description = "Name of the virtual network"
+  type        = string
+  default = "vn-eastus-ci"
+}
+
+variable "address_space" {
+  description = "Address space of the virtual network"
+  type        = list(string)
+  default = [ "10.0.0.0/27" ]
+}
 variable "AZURE_SUBSCRIPTION_ID" {
 
 }
